419b019e0601671983cea309610ac1a86e35a218
madhurichaudhari <madhurichaudhari905@gmail.com>
2020-02-20 17:41:08 +0530

    first time commit

---
+++ src/main/java/com/hcl/ecom/EcommercePoc/ECommercePoc/ECommercePocApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.ecom.EcommercePoc.ECommercePoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommercePocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommercePocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hcl/ecom/EcommercePoc/ECommercePoc/ECommercePocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.ecom.EcommercePoc.ECommercePoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommercePocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
